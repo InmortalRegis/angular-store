bbb68fb7c2ec17e6fc639b2b0aa85eb6d682fa23
Sebastian Manrique <yhoan.manrique@correo.uis.edu.co>
2021-03-22 22:33:15 -0500

    ✨ Learning reactivity and databinding angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-store';
+  item = '';
+  items = ['🍎', '🍏', '🍇', '🍌', '🍑'];
+
+  onKey(event: KeyboardEvent) {
+    if (event.code == 'Enter') {
+      this.addItem();
+    }
+  }
+
+  addItem(event?) {
+    if (!this.item) return;
+    this.items.push(this.item);
+  }
+
+  deleteItem(index: number) {
+    if (confirm('Are you sure you want to delete this item?')) {
+      this.items.splice(index, 1);
+    }
+    return;
+  }
 }
