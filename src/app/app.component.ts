7514df8f7f8f6ab5696e36b0b6e9b5b3cbbd0e24
Sebastian Manrique <yhoan.manrique@correo.uis.edu.co>
2021-03-28 10:47:22 -0500

    feat: product grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,21 +6,69 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  title = '';
   item = '';
   items = ['🍎', '🍏', '🍇', '🍌', '🍑'];
 
-  onKey(event: KeyboardEvent) {
-    if (event.code == 'Enter') {
+  products = [
+    {
+      id: '1',
+      image: 'assets/images/camiseta.png',
+      title: 'Camiseta',
+      price: 80000,
+      description: 'bla bla bla bla bla',
+    },
+    {
+      id: '2',
+      image: 'assets/images/hoodie.png',
+      title: 'Hoodie',
+      price: 80000,
+      description: 'bla bla bla bla bla',
+    },
+    {
+      id: '3',
+      image: 'assets/images/mug.png',
+      title: 'Mug',
+      price: 80000,
+      description: 'bla bla bla bla bla',
+    },
+    {
+      id: '4',
+      image: 'assets/images/pin.png',
+      title: 'Pin',
+      price: 80000,
+      description: 'bla bla bla bla bla',
+    },
+    {
+      id: '5',
+      image: 'assets/images/stickers1.png',
+      title: 'Stickers',
+      price: 80000,
+      description: 'bla bla bla bla bla',
+    },
+    {
+      id: '6',
+      image: 'assets/images/stickers2.png',
+      title: 'Stickers',
+      price: 80000,
+      description: 'bla bla bla bla bla',
+    },
+  ];
+
+  onKey(event: KeyboardEvent): void {
+    if (event.code === 'Enter') {
       this.addItem();
     }
   }
 
-  addItem(event?) {
-    if (!this.item) return;
+  addItem(event?): void {
+    if (!this.item) {
+      return;
+    }
     this.items.push(this.item);
   }
 
-  deleteItem(index: number) {
+  deleteItem(index: number): void {
     if (confirm('Are you sure you want to delete this item?')) {
       this.items.splice(index, 1);
     }
---
+++ src/app/components/product.component.ts
@@ -0,0 +1,29 @@
+import { Component, DoCheck, Input, OnDestroy, OnInit } from '@angular/core';
+import { Product } from '../interfaces/product';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.scss'],
+})
+export class ProductComponent implements OnInit, OnDestroy, DoCheck {
+  @Input() product: Product;
+  constructor() {
+    console.log('1. constructor');
+  }
+
+  // ngOnChanges(changes: SimpleChanges) {
+  //   console.log('2. ngOnChanges');
+  // }
+  ngOnInit(): void {
+    console.log('3. ngOnInit');
+  }
+
+  ngOnDestroy(): void {
+    console.log('5. ngOnDestroy');
+  }
+
+  ngDoCheck(): void {
+    console.log('4. ngDoCheck');
+  }
+}
